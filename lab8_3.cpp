063232200aed206689cf7579901c5781043c9ddd
Pirachach <pirachach_T@cmu.ac.th>
2025-12-19 01:16:43 +0700

    Update lab8_3.cpp

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,40 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<string>
+ using namespace std;
+
+int main(){
+int age,hgt,bty;
+string Mate;
+
+cout << "Enter your age: ";
+cin >> age;
+
+ if(age <=25){
+  cout <<"Enter your height: ";
+cin >> hgt;
+     if(hgt <100){
+     Mate = "Chopper";
+     }
+     else if(hgt <180) {
+         Mate = "Usopp";
+     }
+     else {
+         cout <<"Enter your bounty: ";
+         cin >> bty;
+         if (bty >1100000000) Mate = "Zoro";
+         else Mate = "Sanji";
+  }
+ }
+    else if(age <=60){
+     cout <<"Enter your bounty: ";
+         cin >> bty;
+         if (bty >500000000) Mate = "Jinbe";
+         else Mate = "Franky";
+    }
+    else Mate = "Brook";
+    
+    cout << "Your character = " << Mate<< "\n";
+    return 0;
+}
+  
+
